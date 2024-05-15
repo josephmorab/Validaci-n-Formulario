2b9cb603d1b50dab298ed6f5ffbb934af7e4e7a2
Joseph Mora Barandica <JosephMB@MacBook-Pro-de-Joseph.local>
2024-05-14 22:40:07 -0500

    hicimos un formulario

---
+++ script.js
@@ -0,0 +1,123 @@
+const submitFunction = (event) => {
+    
+    
+    if (!validarFormulario()){
+  
+        event.preventDefault();
+    }
+    else{
+        event.preventDefault();
+        alert(
+            'Los datos enviados fueron: \n' +
+            'Nombre: ' + document.getElementById('nombre').value + '\n'+
+            'Apellido: ' + document.getElementById('apellido').value + '\n'+
+            'Documento: ' + document.getElementById('documento').value + '\n'+
+            'Email: ' + document.getElementById('email').value + '\n'+
+            'Edad: ' + document.getElementById('edad').value + '\n'+
+            'Actividad: ' + document.getElementById('actividad').value + '\n'+
+            'Nivel de Estudio: ' + document.getElementById('nivelEstudio').value + '\n'
+        )
+
+    }
+}
+
+document.getElementById('formulario').addEventListener('submit', submitFunction) // escucha el envío del formulario 
+
+
+
+function validarFormulario() {
+    const camposTexto = document.querySelectorAll('input[type="text"]');
+    let validacionCorrecta = true;
+
+
+    camposTexto.forEach(campo => {
+        let errorCampo = document.getElementById('error' + campo.id.charAt(0).toUpperCase() + campo.id.slice(1))
+        if(campo.value.length == ''){
+            mostrarError(errorCampo, 'Este campo es requerido')
+            validacionCorrecta = false
+
+        } else if(campo.value.length > 0 && campo.value.length < 3) {
+            mostrarError(errorCampo, 'Este campo debe tener al menos 3 caracteres')
+            validacionCorrecta = false
+        }
+        else{
+            ocultarError(errorCampo)
+        }
+    });
+
+
+    const email = document.getElementById('email');
+    let errorEmail = document.getElementById('errorEmail')
+
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) 
+    { //Este regex valida que el formato del email sea valido
+        ocultarError(errorEmail)
+    } else{
+        mostrarError(errorEmail, 'Ingrese un correo eléctronico válido')
+        
+    }
+
+
+
+    // Validación edad 
+
+    const edad = document.getElementById('edad')
+    const errorEdad = document.getElementById('errorEdad')
+
+    if(edad.value < 18){
+        mostrarError(errorEdad,'Debes ser mayor de 18 para registrarte')
+        validacionCorrecta = false
+    } else {
+        ocultarError(errorEdad)
+    }
+    
+//Validación actividad
+
+const actividad = document.getElementById('actividad')
+const errorActividad = document.getElementById('errorActividad')
+
+if (actividad.value == '') {
+    mostrarError(errorActividad, 'Por favor, selecciona una actividad')
+    validacionCorrecta = false
+}else{
+    ocultarError(errorActividad)
+}
+
+
+// Validación de nivel de estudio 
+
+const nivelEstudio = document.getElementById('nivelEstudio')
+const errorNivelEstudio = document.getElementById('errorNivelEstudio')
+
+if(nivelEstudio.value == ''){
+    mostrarError(errorNivelEstudio, 'Por favor, seleccione un nivel de estudio')
+    validacionCorrecta = false
+} else{
+    ocultarError(errorNivelEstudio)
+}
+
+// Validar los terminos y condiciones 
+
+const aceptoTerminos = document.getElementById('aceptoTerminos')
+const errorAceptoTerminos = document.getElementById('errorAceptoTerminos')
+
+if(!aceptoTerminos.checked){
+    mostrarError(errorAceptoTerminos, 'Debes aceptar los términos y condiciones')
+    validacionCorrecta = false
+} else { ocultarError(errorAceptoTerminos)
+
+}
+
+return validacionCorrecta // Esto dira si el formulario es valido 
+
+}
+
+
+const mostrarError = (elemento, mensaje) => {
+    elemento.textContent = mensaje;
+    elemento.style.display = "block";
+}
+const ocultarError = (elemento) => {
+    elemento.textContent = '';
+    elemento.style.display = "none";
+}
